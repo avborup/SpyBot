d5b260f059a36b77c7352b1a64aa0f9f4bede08d
KongBorup <avbroup@gmail.com>
2018-08-25 13:30:25 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const Eris = require("eris");
+const messageCollector = require("./messageCollector.js");
+const config = require("./config.json");
+
+const bot = new Eris(config.token);
+
+bot.on("ready", () => {
+  console.log("Ready to collect data!");
+});
+
+bot.on("messageCreate", async (msg) => {
+  if (msg.content !== config.keyword || msg.author.id !== config.master) return
+
+  const startTime = Date.now();
+
+  const guildChannels = msg.channel.guild.channels;
+  const channels = guildChannels.filter(c => config.channelsToCollectFrom.includes(c.name))
+
+  if (channels.length === 0) {
+    return bot.createMessage(msg.channel.id, "None of the specified channels were found.");
+  }
+
+  const result = await messageCollector.getAllMessagesInChannels(channels);
+
+  const endTime = Date.now();
+  const elapsedTime = endTime - startTime;
+
+  bot.createMessage(msg.channel.id, {
+    embed: {
+        title: "Finished collecting messages",
+        description: `Messages were collected in ${elapsedTime / 1000} seconds`,
+        color: 0xDE1527,
+        fields: [{
+          name: 'Total',
+          value:  `${result.reduce((a, b) => a + b.numOfMsgs, 0)} messages`,
+          inline: true
+        }].concat(result.map(channel => ({
+            name: `#${channel.name}`,
+            value: `${channel.numOfMsgs} messages`,
+            inline: true
+          })))
+      }
+    });
+});
+
+bot.connect();
---
+++ src/messageCollector.js
@@ -0,0 +1,83 @@
+"use strict";
+
+const fs = require("fs");
+const csvStringify = require('csv-stringify');
+
+module.exports.getAllMessagesInChannels = getAllMessagesInChannels;
+
+async function getAllMessagesInChannels(channels) {
+  return new Promise(async (resolve) => {
+    console.log("Started loading");
+
+    const file = fs.createWriteStream(__dirname + "/data_files/msgs.csv");
+    const csvHeaders = [["Timestamp", "Author", "Channel", "Content"]];
+
+    csvStringify(csvHeaders, (err, output) => {
+      if (err) reject(err);
+
+      file.write(output);
+    });
+
+    const result = await Promise.all(channels.map(channel => getAllMessagesInChannel(channel, file)));
+    const totalNumOfMsgs = result.reduce((a, b) => a + b.numOfMsgs, 0);
+
+    console.log(`Finished loading a total of ${totalNumOfMsgs} messages from ${channels[0].guild.name}`);
+
+    // TODO: Figure out why file.end() errors because of
+    // "write after end" without setTimeout().
+    setTimeout(() => {
+      file.end();
+    }, 1);
+
+    resolve(result);
+  });
+}
+
+function getAllMessagesInChannel(channel, file) {
+  return new Promise(async (resolve, reject) => {
+    const firstMessage = (await channel.getMessages(1))[0];
+
+    let id = firstMessage.id;
+    let numOfMsgs = 0;
+    let channelHasMoreMessages = true;
+
+    let promises = [];
+
+    while (channelHasMoreMessages) {
+      // The message sent by the user to initate this command will be ignored
+      const msgs = await channel.getMessages(100, id);
+
+      msgs.forEach(m => {
+        const input = [[m.timestamp, m.author.username, m.channel.name, m.content]];
+
+        csvStringify(input, (err, output) => {
+          if (err) reject(err);
+
+          file.write(output);
+        });
+
+        numOfMsgs++;
+      });
+
+      if (msgs.length < 50) {
+        channelHasMoreMessages = false;
+        continue;
+      }
+
+      id = msgs[msgs.length - 1].id;
+
+      promises.push(new Promise((resolve) => {
+        resolve();
+      }))
+    }
+  
+    Promise.all(promises).then(() => {
+      console.log(`Loaded ${numOfMsgs} messages from #${channel.name}`);
+
+      resolve({
+        name: channel.name,
+        numOfMsgs
+      });
+    });
+  });
+}
